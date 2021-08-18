1f6277c49020ef953de56b09456575a9df6a067a
Kunal Dutta <88330359+Kunalx246@users.noreply.github.com>
2021-08-18 22:18:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function roll_dice(){
+
+var random_Num1=(Math.floor(Math.random()*6)+1);
+var random_Num2=(Math.floor(Math.random()*6)+1);
+
+var link_1="../images/dice"+random_Num1+".png";
+var link_2="../images/dice"+random_Num2+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",link_1);
+document.querySelectorAll("img")[1].setAttribute("src",link_2);
+
+if(random_Num1>random_Num2)
+document.querySelector("h1").innerHTML="Player 1 wins!";
+else if(random_Num1<random_Num2)
+document.querySelector("h1").innerHTML="Player 2 wins!";
+else
+document.querySelector("h1").innerHTML="Draw!";
+}
